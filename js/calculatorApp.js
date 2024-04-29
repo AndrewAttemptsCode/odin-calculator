99c10949088b9aa41cbd87f39d9f09a30331ee6e
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-04-29 14:52:28 +0100

    Add operate function

--- js/calculatorApp.js
+++ js/calculatorApp.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+// let operandFirst = "";
+// let operandSecond = "";
+// let operator = "";
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+        default:
+            return "Invalid operator";
+    }
+}
